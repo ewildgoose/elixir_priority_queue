ed25e2245efc7778178b74fea16e0bd858689fbd
Ed Wildgoose <git@wildgooses.com>
2016-04-15 21:08:59 +0100

    Hopefully got the docs deps correct now...

--- mix.exs
+++ mix.exs
@@ -30,7 +30,8 @@ defmodule PriorityQueue.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:ex_doc, github: "elixir-lang/ex_doc", only: :docs}]
+    [{:earmark, "~> 0.2.0", only: :docs},
+     {:ex_doc, "~> 0.11.0", only: :docs}]
   end
 
   defp package do
