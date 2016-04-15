bc2874dbe1703ce48f83e7b1be9b807409b203e3
Ed Wildgoose <git@wildgooses.com>
2016-04-15 20:56:25 +0100

    ex_doc only needed for docs

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule PriorityQueue.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    [{:ex_doc, github: "elixir-lang/ex_doc"}]
+    [{:ex_doc, github: "elixir-lang/ex_doc", only: :docs}]
   end
 
   defp package do
